a2239a3312c61ba004480e68cce7df0baf2a04cd
yucadag <yucadag@gmail.com>
2018-12-23 17:42:14 +0300

    First Commit

---
+++ Lessons/src/Variables.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author mustafayucadag
+ *
+ */
+public class Variables {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
