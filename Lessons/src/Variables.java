8ea37b200d0c1bce25900b3304f7145e28465dc2
yucadag <yucadag@gmail.com>
2018-12-23 17:43:33 +0300

    Variables Lessons Add

--- Lessons/src/Variables.java
+++ Lessons/src/Variables.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -13,7 +15,15 @@ public class Variables {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 Scanner input=new Scanner(System.in);
+		    
+ 		int sayi;
+ System.out.println("Bir sayi giriniz");
+ sayi=input.nextInt();
+ input.close();
+ System.out.println(sayi*5);
+ System.out.println("Tebrikler Carpimi buldunuz..");
+ System.out.println("Meyra icin hersey!");
 	}
 
 }
